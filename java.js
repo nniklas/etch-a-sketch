1eb8a2437ea7ad37f9915b9c53ff1cc9d0dfe9a6
nniklas <46904141+nniklas@users.noreply.github.com>
2025-04-04 11:25:00 +0200

    add pseudocode for functions and logic

--- java.js
+++ java.js
@@ -1,2 +1,38 @@
 const container = document.querySelector("#container")
 const div = document.createElement("div")
+
+// define needed functions 
+// a function that creates a div
+
+
+// a function that appends an element to another
+// use this to append child divs in the grid to the container
+// inputs child and parent elements
+
+// a function (attachAttribute) that creates attributes to elements
+
+
+// a function that creates a button
+
+// a function that asks for user input
+
+// a function that checks user input and only return numeric values
+// return number that was entered or false if invalid input
+
+
+//Logic
+
+// define maxgridsize
+
+// use attacheAttribute to make divs in grid attain classes from stylesheet
+
+// use attacheAttribute to make button attain desired attributes
+
+// ask for user input on new grid size
+
+// if invalid input ask again
+
+// if valid remove current grid (if any)
+
+// create new grid 
+
