02e914365fab327a450fcba8005b2455f56619ac
nniklas <46904141+nniklas@users.noreply.github.com>
2025-04-06 15:45:21 +0200

    bug fix trying to make squares square, not working

--- java.js
+++ java.js
@@ -47,7 +47,7 @@ let maxGridsize = 100;
 let gridSize = 0;
 
 // only use this variable temporarily to check code without user inputs
-let tempUserInput = 50;
+let tempUserInput = 5;
 
 //Logic
 
@@ -62,14 +62,14 @@ for (let i=0; i < tempUserInput; i++){
     // create row container elememts using createEL function
     let divContainer = createEl("div");
     appendChildElement(divContainer,container);
-    attachAttribute(divContainer, "class",".columnContainer");
+    attachAttribute(divContainer, "class","mainContainer");
+    // divContainer.textContent = `${i+1}`;
 
     // loop through for each row to create grid elements
     for (j=0; j< tempUserInput; j++){
         let div = createEl("div");
         div.textContent = `${i+1}`;
         appendChildElement(div,divContainer);
-        attachAttribute(div, "class",".gridSquare");
     };
 };
 
